e979fdb8b206da37cd16780b790e659bc7a8af5d
Shawn Looker <shawn.looker@gmail.com>
2016-06-07 08:53:30 -0400

    Ain2 wrapper.

---
+++ README.md
@@ -0,0 +1,27 @@
+== Ain 2 Wrapper ==
+
+This just creates a simple wrapper around ain2 that instantiates an object.  It was created mostly because I didn't want to override console. 
+
+You can use it like below:
+
+~~~~
+let logger = require('ain2-wrapper')();
+
+logger.warn("This is a warning.");
+// This will output "WARN - This is a warning."
+~~~~
+
+This supports the same log levels as ain.  Severity level is referenced to [RFC3164](http://www.faqs.org/rfcs/rfc3164.html)
+
+~~~~
+#  String   Description
+-----------------------
+0  emerg    Emergency: system is unusable
+1  alert    Alert: action must be taken immediately
+2  crit     Critical: critical conditions
+3  err      Error: error conditions
+4  warn     Warning: warning conditions
+5  notice   Notice: normal but significant condition
+6  info     Informational: informational messages
+7  debug    Debug: debug-level messages
+~~~~
---
+++ index.js
@@ -0,0 +1,46 @@
+const SysLogger = require('ain2');
+const logger = new SysLogger({tag: config.appName, facility: 'syslog'});
+
+// This is an INCREDIBLY basic wrapper.  When you require it, you need to do require('logger')();  Do not forget the extra parens.
+
+module.exports = function() {
+  return {
+    emerge: (message) => { logMessage('emerge', message) },
+    alert: (message) => { logMessage('alert', message) },
+    crit: (message) => { logMessage('crit', message) },
+    err: (message) => { logMessage('err', message) },
+    warn: (message) => { logMessage('warn', message) },
+    notice: (message) => { logMessage('notice', message) },
+    info: (message) => { logMessage('info', message) },
+    debug: (message) => { logMessage('debug', message) }
+  };
+}
+
+function logMessage(level, message) {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    // If in development mode, console.log it
+    console.log(`${level.toUpperCase()} - ${message}`);
+  } else {
+    // Send to syslog if not in development
+    switch (level) {
+      case 'emerge':
+        syslog.emerge(message);
+      case 'alert':
+        syslog.alert(message);
+      case 'crit':
+        syslog.crit(message);
+      case 'err':
+        syslog.err(message);
+      case 'warn':
+        syslog.warn(message);
+      case 'notice':
+        syslog.notice(message);
+      case 'info':
+        syslog.info(message);
+      case 'debug':
+        syslog.debug(message);
+      default:
+        syslog.debug(message);
+    }
+  }
+}
